34a84cda6955cc0f849ffaac43843e062482d243
Luan <83055293+Luanzinhob5@users.noreply.github.com>
2025-08-13 15:55:40 -0300

    feat(manipulacao-de-erros)

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,12 @@ def criar_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     db.init_app(app)
-    
 
+    from . import errors
+    app.register_error_handler(404, errors.handle_not_found)
+    app.register_error_handler(400, errors.handle_bad_request)
+    app.register_error_handler(500, errors.handle_internal_server_error)
+    
     from .rotas_posts import posts_bp
     app.register_blueprint(posts_bp, url_prefix='/api')
     
